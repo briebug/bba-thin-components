df54a86384620ea982c3efba6a1a869dfd1c9448
Lukas Ruebbelke <lukas.ruebbelke@briebug.com>
2020-11-17 16:44:04 -0700

    first commit

---
+++ jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  projects: [
+    '<rootDir>/apps/api',
+    '<rootDir>/libs/api-interfaces',
+    '<rootDir>/libs/material',
+    '<rootDir>/libs/ui-toolbar',
+    '<rootDir>/apps/client',
+  ],
+};
